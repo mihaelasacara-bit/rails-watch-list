6e811f27b98cd41b0172bcb46a4dcbe6b5d38657
Mihaela Sacara <mihaelasacara@gmail.com>
2026-04-09 14:50:57 +0200

    index, new and create routes, actions and views created

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,24 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to "/lists", notice: "List was created."
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def list_params
+    params.expect(list: [ :name ])
+  end
+end
